2b48757f84f65cdba4373fb6e0235ad8bfc386e4
Christian Escobar <christian.e@rompecabeza.cl>
2020-07-22 23:07:12 -0400

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { SelectComponent } from './components/home/select/select.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SelectComponent
   ],
   imports: [
     BrowserModule
